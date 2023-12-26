444b45c8f8c07001a30c00b00ae8d4521bd6fe4b
bik1111 <bik1111@khu.ac.kr>
2023-12-26 20:18:31 +0900

    fix : crawling code & Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,26 @@
-# Use python:3-slim as a base image
-FROM python:3.9-slim
-
-# Set the working directory in the container
+FROM --platform=linux/amd64  python:3.9
 WORKDIR /app
+RUN apt-get update
 
-# Copy the current directory contents into the container at /app
+# Install Chrome
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+RUN apt-get -y update
+RUN apt-get install -y google-chrome-stable
+
+# Check chrome version
+RUN google-chrome --version
+
+# Install ChromeDriver.
+RUN apt-get install wget
+RUN apt-get install -yqq unzip
+RUN wget -O /tmp/chromedriver.zip https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/linux64/chromedriver-linux64.zip
+
+RUN mkdir chrome
+RUN unzip /tmp/chromedriver.zip chromedriver-linux64/chromedriver -d /app/chrome
+
 COPY . .
 
-# Install Git
-RUN apt-get update && apt-get install -y git
+RUN pip install --no-cache-dir -r requirements.txt
 
-# Install any needed packages specified in requirements.txt
-RUN pip install -r requirements.txt
-
-# Make port 80 available to the world outside this container
-EXPOSE 3000
-
-# Run app.py when the container launches
-CMD ["python3", "crawling.py"]
+CMD [ "python3", "crawling.py" ]
--- crawling/crawling.py
+++ crawling/crawling.py
@@ -1,14 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import os
-import re
 import csv
 import requests
-#from pykospacing import Spacing
 import torch
 from sentence_transformers import SentenceTransformer, util
 import pandas as pd
+import ast
 import time
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -19,17 +17,32 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from sentence_transformers import SentenceTransformer
 
+chrome_options = Options()
+
+#for Docker
+#chrome_options.add_argument('headless')
+#chrome_options.add_argument('window-size=1920x1080')
+#chrome_options.add_argument("disable-gpu")
+#chrome_options.add_argument(f'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36')
+
+#chromedriver_path = "/app/chrome/chromedriver"
+#driver = webdriver.Chrome(executable_path = chromedriver_path, options=chrome_options)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+
+data_list = []
 
 def crawl_page(url):
-    chrome_options = Options()
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(4)
+
+    driver.implicitly_wait(3)
     driver.get(url)
 
     last_height = driver.execute_script("return document.body.scrollHeight")
 
     for page_number in range(1, 7):  # Crawl up to 10 pages
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
         new_height = driver.execute_script("return document.body.scrollHeight")
 
         page_source = driver.page_source
@@ -57,12 +70,11 @@ def crawl_page(url):
         next_page_selector = f"#content > div.style_content__xWg5l > div.pagination_pagination__fsf34 > div > a:nth-child({page_number + 1})"
         next_page_button = driver.find_element(By.CSS_SELECTOR, next_page_selector)
         next_page_button.click()
-
+        time.sleep(2)
         last_height = new_height
 
     driver.quit()
 
-data_list = []
 
 def review_crawl():
     for item in data_list:
@@ -73,15 +85,14 @@ def review_crawl():
         review_soup = BeautifulSoup(response.content, 'html.parser')
         # 리뷰 아이템 선택
         reviews = review_soup.select('p.reviewItems_text__XrSSf')
-        # 정규 표현식을 사용하여 한글만 남기기
-        cleaned_reviews = [re.sub(r'[^가-힣\s]', '', review.get_text()) for review in reviews]
+        time.sleep(2)
 
         # 리뷰를 해당 상품의 딕셔너리에 추가
-        item['reviews'] = cleaned_reviews
+        item['reviews'] = reviews
+        time.sleep(1)
 
 
 # Initial URL
-initial_url = "https://search.shopping.naver.com/search/category/100005307"
 
 def save_to_csv(data_list, file_path='output.csv'):
     with open(file_path, mode='w', encoding='utf-8', newline='') as file:
@@ -94,22 +105,34 @@ def save_to_csv(data_list, file_path='output.csv'):
         writer.writerows(data_list)
 
 
+# ### 3-1) 리뷰 수집 및 한글만 남기기
+def review_crawl():
+    for item in data_list:
+        link_url = item['product_link_url']
 
+        # 새로운 HTTP 요청을 보내서 리뷰 페이지를 크롤링
+        response = requests.get(link_url)
+        review_soup = BeautifulSoup(response.content, 'html.parser')
+        # 리뷰 아이템 선택
+        reviews = review_soup.select('p.reviewItems_text__XrSSf')
+        time.sleep(1)
+        # 정규 표현식을 사용하여 한글만 남기기
+        cleaned_reviews = [re.sub(r'[^가-힣\s]', '', review.get_text()) for review in reviews]
 
-# ### 3-1) 한글만 남기기
-def extract_word(text):
-    hangul = re.compile('[^가-힣]')
-    result = hangul.sub(' ', text)
-    return result
+        # 리뷰를 해당 상품의 딕셔너리에 추가
+        item['reviews'] = cleaned_reviews
+        time.sleep(2)
 
-
-
+# TOP 5 추천 상품
 def get_query_sim_top_k(query, model, df, top_k):
     # Encode the query
     query_encode = model.encode(query)
 
+    # Convert string representations of lists to actual lists
+    df['hf_embeddings'] = df['hf_embeddings'].apply(ast.literal_eval)
+
     # Compute cosine similarity
-    cos_scores = util.pytorch_cos_sim(query_encode, df['hf_embeddings'])[0]
+    cos_scores = util.pytorch_cos_sim(query_encode, torch.tensor(df['hf_embeddings'].tolist()))[0]
 
     # Get top k results
     top_results_idx = torch.topk(cos_scores, k=top_k).indices.cpu().numpy()
@@ -118,32 +141,40 @@ def get_query_sim_top_k(query, model, df, top_k):
     return top_results_df
 
 
+initial_url = "https://search.shopping.naver.com/search/category/100005307"
+model = SentenceTransformer("jhgan/ko-sroberta-multitask")
 
 if __name__ == "__main__":
+
+    # 크롤링 시작
     crawl_page(initial_url)
     time.sleep(3)
+
+    # 리뷰 크롤링
     print("start review crawling")
     review_crawl()
+
+    # CSV 파일로 저장
     save_to_csv(data_list)
 
+    # CSV 파일 읽기
+    print("start read csv")
     df = pd.read_csv('output.csv')
-    print("start extract word")
-    df['reviews'] = df['reviews'].apply(lambda x: extract_word(x))
 
-    #spacing = Spacing()
-    #print("start spacing")
-    #df['reviews'] = df['reviews'].apply(lambda x: spacing(x))
+    # Convert the 'reviews' column from string representation to actual lists
+    df['reviews'] = df['reviews'].apply(eval)
 
+    # Embedding
+    sentence_embeddings = model.encode(df['reviews'].apply(lambda x: " ".join(x)).to_numpy().tolist())
 
-    # # 3. Embeding Vector 추출하기
-    # Initialize the SentenceTransformer model
-    embedder = SentenceTransformer("jhgan/ko-sroberta-multitask")
-    print("start embedding")
-    df['hf_embeddings'] = df['reviews'].apply(lambda x : embedder.encode(x))
+    # Convert the embeddings to a list and assign to 'hf_embeddings' column
+    df['hf_embeddings'] = sentence_embeddings.tolist()
 
-    model = SentenceTransformer("jhgan/ko-sroberta-multitask")
+    # Save the DataFrame to a new CSV file
+    df.to_csv('output.csv', index=False)
 
-    get_query_sim_top_k("대학생들이 사용하기에 좋은 노트북", model, df, 5)
+    #cosine similarity 구하기
+    print(get_query_sim_top_k("대학생들이 사용하기에 좋은 노트북", model, df, 5))
 
 
 
